a70f34672771b1ad15c20912134f04dc5daa5628
Andrea Mariani <and.mariani3@stud.uniroma3.it>
2021-07-26 13:41:33 +0200

    initial commit

---
+++ generate_tree.py
@@ -0,0 +1,97 @@
+import json
+from web3 import Web3
+from flask import Flask, json, request
+from flask_cors import CORS
+
+class Tree(dict):
+    def __init__(self, name, children=None):
+        super().__init__()
+        self.__dict__ = self
+        self.name = name
+        if children is not None:
+            self.children = list(children)
+        else:
+            self.children = []
+
+web3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/33319769ccc74511ac1eb90012e6f04c'))
+if (web3.isConnected()):
+    print("Connection successful" + "\n")
+else:
+    print("Connection refused" + "\n")
+
+uncle_to_append = {}
+
+def generate_tree(num_block):
+    tree_before = generate_tree_aux(num_block -5, 5, num_block)
+    tree_after = generate_tree_aux(num_block +1, 3, num_block)
+
+    junction_of_trees(tree_before, tree_after, num_block)
+
+    # è stato appeso il tree_after al tree_before, quindi da adesso in poi si considera il tree_before che rappresenta tutto l'albero
+    append_uncle(tree_before, uncle_to_append)
+
+    string_of_final_tree = json.dumps(tree_before.__dict__)
+    
+    return string_of_final_tree
+    # json_file = open("./data/data.json", "w")
+    # json_file.write(string_of_final_tree)
+    # json_file.close()
+
+def generate_tree_aux(root, height, length):
+
+    root_hash = web3.eth.getBlock(root)["hash"].hex()[:7]
+    number_uncles = web3.eth.get_uncle_count(root)
+
+    tree = Tree(root_hash, [])
+
+    if height == 0:
+        for i in range(number_uncles):
+            uncle = web3.eth.get_uncle_by_block(root, i)
+            fork_height = web3.eth.getBlock(uncle["parentHash"])["number"]
+            if fork_height >= length:
+                if uncle["parentHash"][:7] not in uncle_to_append:
+                    uncle_to_append[uncle["parentHash"][:7]] = [Tree(uncle["hash"][:7], [])]
+                else:
+                    uncle_to_append[uncle["parentHash"][:7]] = uncle_to_append[uncle["parentHash"][:7]] + [Tree(uncle["hash"][:7], [])]
+        return tree
+    else: 
+        tree.children.append(generate_tree_aux(root + 1, height -1, length))
+        for i in range(number_uncles):
+            uncle = web3.eth.get_uncle_by_block(root, i)
+            fork_height = web3.eth.getBlock(uncle["parentHash"])["number"]
+            if fork_height >= length :
+                if uncle["parentHash"][:7] not in uncle_to_append:
+                    uncle_to_append[uncle["parentHash"][:7]] = [Tree(uncle["hash"][:7], [])]
+                else:
+                    uncle_to_append[uncle["parentHash"][:7]] = uncle_to_append[uncle["parentHash"][:7]] + [Tree(uncle["hash"][:7], [])]
+        return tree
+
+# si visita l'albero e quando si arriva al punto giusto si fa la giunzione
+def junction_of_trees(tree_before, tree_after, num_block):
+    if tree_before.name == web3.eth.getBlock(num_block)["hash"].hex()[:7]:
+        tree_before.children.insert(0, tree_after)
+    else:
+        junction_of_trees(tree_before.children[0], tree_after, num_block)
+
+def append_uncle(tree, uncle_to_append):
+    for key in uncle_to_append:
+        if key == tree.name:
+            for e in uncle_to_append[key]:
+                tree.children.append(e)            
+    for child in tree.children:
+        append_uncle(child, uncle_to_append)
+
+# generate_tree(122)
+
+api = Flask(__name__)
+CORS(api)
+
+@api.route('/generate-tree', methods=['GET'])
+def get_companies():
+    n = request.args.get('height')
+    return generate_tree(int(n))
+
+api.run()
+# prima era scritto così
+# if __name__ == '__main__':
+#     api.run()
---
+++ js/main.js
@@ -0,0 +1,188 @@
+/**
+ * Creato da Gianluca Mariani e Andrea Mariani il 13/07/2021 
+ */
+
+var dy = 110;
+var dx = 160;
+var width = 5000;
+var height = 6000;
+
+var div = d3.select("div")
+            .append("div")
+            .classed('containerAlbero', true);
+
+var svg = d3.select("div")
+            .select("div")
+            .append("svg")
+            .classed('viewContainer', true)
+            .style("font", "16px sans-serif")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("id", "svg");
+
+// ragionare un attimo su come mettere queste variabili anche in base al progetto precedente
+var tree = d3.tree().nodeSize([dx, dy]);
+
+var gNode = svg.append("g")
+               .attr("cursor", "pointer")
+               .attr("pointer-events", "all");
+
+var gLink = svg.append("g")
+               .attr("fill", "none")
+               .attr("stroke", "#555")
+               .attr("stroke-opacity", 0.4)
+               .attr("stroke-width", 1.5);
+
+function updateDraw(root) {
+    var nodes = root.descendants();
+    var links = root.links();
+    tree(root);
+
+    root.descendants().forEach(node => {
+        node.x = node.x + height/2;
+        node.y = node.y + width/2;
+    });
+
+    // clausola update per i nodi
+    var node = gNode.selectAll("g")
+                    .data(nodes, d => d.id);
+
+    // clausola enter per i nodi
+    var nodeEnter = node.enter()
+                        .append("g")
+                        .attr("transform", d => `translate(${d.y},${d.x})`)
+                        .attr("fill-opacity", function(d) {
+                            if (d.data.name == "nullo")
+                                return 0;
+                            else
+                                return 1;
+                        })
+                        .attr("stroke-opacity", 1)
+                        .on("click", function(d) {
+                            click(d, root);
+                        });
+
+    // non si può appendere direttamente sopra, perché tutte queste cose vanno appese all'oggetto restituito sopra
+    nodeEnter.append("rect")
+             .attr("width", 20)
+             .attr("height", 20)
+             .attr("fill", function(d) {
+                 var flag = "false";
+                 if (d._children) {
+                     d._children.forEach(figlio => {
+                         if (figlio.data.name != "nullo")
+                             flag = "true";
+                     })
+                 }
+                 if (flag == "false")
+                     return "#999";
+                 else
+                     return "#555";
+             });
+
+    // non si può appendere direttamente sopra, perché tutte queste cose vanno appese all'oggetto restituito sopra
+    nodeEnter.append("text")
+             .attr("dy", "-0.5em")
+             .attr("x", d => d._children ? 35 : -6)
+             .attr("text-anchor", d => d._children ? "end" : "end")
+             .text(function(d) {
+                 if (d.data.name == "nullo")
+                     return "";
+                 else
+                     return d.data.name;
+             })
+             .clone(true).lower()
+             .attr("stroke-linejoin", "round");
+
+    // clausola exit per i nodi
+    node.exit().remove();
+
+    // clausola update per i link
+    var link = gLink.selectAll("path")
+                    .data(links, d => d.target.id);
+
+    // clausola enter per i link
+    link.enter()
+        .append("path")
+        .attr("d", d3.linkHorizontal().x(d => d.y)
+                                        .y(d => d.x))
+        .attr("stroke-opacity", d => d.target.data.name == "nullo" ? 0 : 0.5);
+
+    // clausola exit per i link
+    link.exit().remove();
+
+}
+
+function click(d, root) {
+    // prova a richiamare updateDraw con d invece che con root e vedi che succede, magari serve per capire il codice
+    var flag = "false";
+    if (d.children != null) {
+        d.children = null;
+        updateDraw(root);
+    } else {
+        var flag = "false";
+        if (d._children) {
+            d._children.forEach(figlio => {
+                if (figlio.data.name != "nullo")
+                    flag = "true";
+            })
+            if (flag == "true") {
+                d.children = d._children;
+                updateDraw(root);
+            }
+        }
+
+    }
+}
+
+function myFunction() {
+    d3.select("div").select("div").select("svg").selectAll("g").selectAll('*').remove();
+    // viene utilizzata la libreria JQuery per interrogare gli elementi del file html e dirgli che lo scroll del div deve essere messo centralmente
+    $(document).ready(function() {
+        $(document).ready(function() {
+            var outerContent = $('.containerAlbero');
+            var innerContent = $('.viewContainer');
+            outerContent.scrollLeft((innerContent.width() - outerContent.width()) / 2);
+            outerContent.scrollTop((innerContent.height() - outerContent.height()) / 2);
+        });
+    });
+    var n = document.getElementById("num").value;
+    axios.get("http://localhost:5000/generate-tree?height=" + n)
+    .then(function(data){
+        // queste due istruzioni vanno fatto qua perché si devono "far scomparire i figli"
+        var root = d3.hierarchy(data.data);
+
+        root.descendants()
+            .forEach(function(d, i) {
+                d.id = i;
+                d._children = d.children;
+                d.children = null;
+            });
+
+        // aggiornamento del disegno
+        updateDraw(root);
+
+  }).catch(function(error) {
+        console.log("errore altezza");
+    });;
+}
+
+// d3.json("data/data.json")
+//   .then(function(data) {
+//         // queste due istruzioni vanno fatto qua perché si devono "far scomparire i figli"
+//         var root = d3.hierarchy(data);
+
+//         root.descendants()
+//             .forEach(function(d, i) {
+//                 d.id = i;
+//                 d._children = d.children;
+//                 d.children = null;
+//             });
+
+//         // aggiornamento del disegno
+//         updateDraw(root);
+
+//   })
+//   .catch(function(error) {
+//         console.log(error);
+//   });
