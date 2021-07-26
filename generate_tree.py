72d5b3d7cb4131c8529d9abfc6bda4a984ae8780
Andrea Mariani <and.mariani3@stud.uniroma3.it>
2021-07-26 14:39:06 +0200

    another fix to generate_tree.py

--- generate_tree.py
+++ generate_tree.py
@@ -19,69 +19,48 @@ if (web3.isConnected()):
 else:
     print("Connection refused" + "\n")
 
-uncle_to_append = {}
+blocks_to_append = {}
 
 def generate_tree(num_block):
-    tree_before = generate_tree_aux(num_block - 5, 5, num_block - 5)
-    tree_after = generate_tree_aux(num_block + 1, 3, num_block - 5)
+    generated_tree = generate_tree_aux(num_block - 5, 9, num_block - 5)
 
-    junction_of_trees(tree_before, tree_after, num_block)
+    append_blocks(generated_tree, blocks_to_append)
 
-    # è stato appeso il tree_after al tree_before, quindi da adesso in poi si considera il tree_before che rappresenta tutto l'albero
-    append_uncle(tree_before, uncle_to_append)
-
-    string_of_final_tree = json.dumps(tree_before.__dict__)
+    string_of_tree = json.dumps(generated_tree.__dict__)
     
-    return string_of_final_tree
-    # json_file = open("./data/data.json", "w")
-    # json_file.write(string_of_final_tree)
-    # json_file.close()
+    return string_of_tree
 
 def generate_tree_aux(root, height, height_root):
-
     root_hash = web3.eth.getBlock(root)["hash"].hex()[:7]
-    number_uncles = web3.eth.get_uncle_count(root)
+    uncles_number = web3.eth.get_uncle_count(root)
 
     tree = Tree(root_hash, [])
 
     if height == 0:
-        for i in range(number_uncles):
-            uncle = web3.eth.get_uncle_by_block(root, i)
-            fork_height = web3.eth.getBlock(uncle["parentHash"])["number"]
-            if fork_height >= height_root:
-                if uncle["parentHash"][:7] not in uncle_to_append:
-                    uncle_to_append[uncle["parentHash"][:7]] = [Tree(uncle["hash"][:7], [])]
-                else:
-                    uncle_to_append[uncle["parentHash"][:7]] = uncle_to_append[uncle["parentHash"][:7]] + [Tree(uncle["hash"][:7], [])]
+        generate_blocks_to_append(root, uncles_number, height_root)
         return tree
     else: 
         tree.children.append(generate_tree_aux(root + 1, height -1, height_root))
-        for i in range(number_uncles):
-            uncle = web3.eth.get_uncle_by_block(root, i)
-            fork_height = web3.eth.getBlock(uncle["parentHash"])["number"]
-            if fork_height >= height_root:
-                if uncle["parentHash"][:7] not in uncle_to_append:
-                    uncle_to_append[uncle["parentHash"][:7]] = [Tree(uncle["hash"][:7], [])]
-                else:
-                    uncle_to_append[uncle["parentHash"][:7]] = uncle_to_append[uncle["parentHash"][:7]] + [Tree(uncle["hash"][:7], [])]
+        generate_blocks_to_append(root, uncles_number, height_root)
         return tree
 
-# si visita l'albero e quando si arriva al punto giusto si fa la giunzione
-def junction_of_trees(tree_before, tree_after, num_block):
-    if tree_before.name == web3.eth.getBlock(num_block)["hash"].hex()[:7]:
-        tree_before.children.insert(0, tree_after)
-    else:
-        junction_of_trees(tree_before.children[0], tree_after, num_block)
+def generate_blocks_to_append(root, uncles_number, height_root):
+    for i in range(uncles_number):
+        uncle = web3.eth.get_uncle_by_block(root, i)
+        fork_height = web3.eth.getBlock(uncle["parentHash"])["number"]
+        if fork_height >= height_root:
+            if uncle["parentHash"][:7] not in blocks_to_append:
+                blocks_to_append[uncle["parentHash"][:7]] = [Tree(uncle["hash"][:7], [])]
+            else:
+                blocks_to_append[uncle["parentHash"][:7]] = blocks_to_append[uncle["parentHash"][:7]] + [Tree(uncle["hash"][:7], [])]
 
-def append_uncle(tree, uncle_to_append):
-    for key in uncle_to_append:
+def append_blocks(tree, blocks_to_append):
+    for key in blocks_to_append:
         if key == tree.name:
-            for e in uncle_to_append[key]:
+            for e in blocks_to_append[key]:
                 tree.children.append(e)            
     for child in tree.children:
-        append_uncle(child, uncle_to_append)
-
-# generate_tree(122)
+        append_blocks(child, blocks_to_append)
 
 api = Flask(__name__)
 CORS(api)
@@ -92,6 +71,3 @@ def get_companies():
     return generate_tree(int(n))
 
 api.run()
-# prima era scritto così
-# if __name__ == '__main__':
-#     api.run()
